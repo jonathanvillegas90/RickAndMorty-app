582cebee8a25bb32d8e4e2e83a101282fbbb2730
Jonathan Villegas <77998536+jonathanvillegas90@users.noreply.github.com>
2022-06-15 19:09:14 -0300

    cards con funcionalidad creadas

--- client/src/app/utility/card/card.component.ts
+++ client/src/app/utility/card/card.component.ts
@@ -6,5 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-  text = 'nombre pero soy una cosa barbara';
+  characters = [
+    {
+      id: 1,
+      name: 'Ricardo Sanchez',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+      text: 'el mas poronga',
+    },
+    {
+      id: 2,
+      name: 'Ricardo Sanchez tierra 616',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+    },
+    {
+      id: 3,
+      name: 'Ricardo Sanchez tierra 90',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+    },
+    {
+      id: 4,
+      name: 'Morty Malvado',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+    },
+    {
+      id: 4,
+      name: 'Morty Bueno',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+    },
+    {
+      id: 4,
+      name: 'Morty ',
+      img: 'https://www.geekmi.news/__export/1626821131805/sites/debate/img/2021/07/20/rick4_2.jpg_976912859.jpg',
+      type: 'Human',
+    },
+  ];
 }
